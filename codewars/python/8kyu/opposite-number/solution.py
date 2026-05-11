54421048e2e0efdab246a482366e4829e33a4086
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-11 17:48:11 -0300

    Create opposite-number

---
+++ codewars/python/8kyu/opposite-number/solution.py
@@ -0,0 +1,14 @@
+# Very simple, given a number, find its opposite (additive inverse)
+# Examples:
+# 1: -1
+# 14: -14
+# -34: 34
+
+def opposite(number):
+    return -number
+
+
+if __name__ == "__main__":
+    print(opposite(1))  # -1
+    print(opposite(14))  # -14
+    print(opposite(-34))  # 34
