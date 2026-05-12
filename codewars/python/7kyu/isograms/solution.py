41c2702477e67221849aebda32c5b1e943b6ad22
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-12 18:58:51 -0300

    Create isograms

---
+++ codewars/python/7kyu/isograms/solution.py
@@ -0,0 +1,17 @@
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+# Example: (Input --> Output)
+
+# "Dermatoglyphics" --> true
+# "aba" --> false
+# "moOse" --> false (ignore letter case)
+
+
+def is_isogram(string):
+    return sum([string.lower().count(x) for x in string.lower()]) == len(string)
+
+
+if __name__ == "__main__":
+    print(is_isogram("abcdef"))  # True
+    print(is_isogram("123456"))  # True
+    print(is_isogram("this is not"))  # False
