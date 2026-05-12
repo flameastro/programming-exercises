0ecdc0e2b440e1a1073bb37e01327439aa6e901a
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-12 14:35:04 -0300

    Create reversed-strings

---
+++ codewars/python/8kyu/reversed-strings/solution.py
@@ -0,0 +1,11 @@
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+def solution(string):
+    return string[::-1]
+
+
+if __name__ == "__main__":
+    print(solution("world"))  # dlrow
+    print(solution("codewars"))  # srawedoc
+    print(solution("reversed string"))  # gnirts desrever
