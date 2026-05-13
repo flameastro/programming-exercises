ef6e84c9d0ac7e1a847778e0342d8794198b4209
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-13 17:23:11 -0300

    Create you-cant-code-under-pressure

---
+++ codewars/python/8kyu/you-cant-code-under-pressure/solution.py
@@ -0,0 +1,9 @@
+# Code as fast as you can! You need to double the integer and return it.
+def double_integer(i):
+    return i * 2
+
+
+if __name__ == "__main__":
+    print(double_integer(2))  # 4
+    print(double_integer(3))  # 6
+    print(double_integer(6))  # 12
