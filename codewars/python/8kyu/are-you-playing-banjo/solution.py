1f7d42fbb1be95df9c1e44fbca0f787e653a82cc
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-13 15:27:22 -0300

    Create are-you-playing-banjo

---
+++ codewars/python/8kyu/are-you-playing-banjo/solution.py
@@ -0,0 +1,19 @@
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+# The function takes a name as its only argument, and returns one of the following strings:
+# name + " plays banjo" 
+# name + " does not play banjo"
+# Names given are always valid strings
+# Solution 1
+def are_you_playing_banjo(name):
+    return f"{name} plays banjo" if name[0].lower() == "r" else f"{name} does not play banjo"
+
+
+# Solution 2
+def are_you_playing_banjo(name):
+    return f"{name} plays banjo" if name.lower().startswith("r") else f"{name} does not play banjo"
+
+if __name__ == "__main__":
+    print(are_you_playing_banjo("Rafael"))  # Rafael plays banjo
+    print(are_you_playing_banjo("Naditisky"))  # Naditisky does not play banjo
+    print(are_you_playing_banjo("Ainkisted"))  # Ainkisted does not play banjo
