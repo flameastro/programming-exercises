aefbb7cf9d361346d37aed954217e577e65a01af
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-13 17:50:55 -0300

    Create returning-strings

---
+++ codewars/python/8kyu/returning-strings/solution.py
@@ -0,0 +1,8 @@
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+
+if __name__ == "__main__":
+    print(greet("Adrian"))  # Hello, Adrian how are you doing today?
+    print(greet("flameastro"))  # Hello, flameastro how are you doing today?
+    print(greet("Juan"))  # Hello, Juan how are you doing today?
