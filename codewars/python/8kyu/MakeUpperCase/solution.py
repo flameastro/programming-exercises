1b09aa1f339097e7464f3cc7913350373e495c17
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-13 17:42:45 -0300

    Create MakeUpperCase

---
+++ codewars/python/8kyu/MakeUpperCase/solution.py
@@ -0,0 +1,9 @@
+# Write a function which converts the input string to uppercase.
+def make_upper_case(s):
+    return s.upper()
+
+
+if __name__ == "__main__":
+    print(make_upper_case("abcdef"))  # ABCDEF
+    print(make_upper_case("testing the code"))  # TESTING THE CODE
+    print(make_upper_case("is this gonna be UPPERCASE?"))  # IS THIS GONNA BE UPPERCASE?
