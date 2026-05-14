5e8792af7f91ad42c7785e3c261dd0aeb471da0f
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-14 20:19:51 -0300

    Create squaring-an-argument

---
+++ codewars/python/8kyu/squaring-an-argument/solution.py
@@ -0,0 +1,11 @@
+# Now you have to write a function that takes an argument and returns the square of it.
+def square(n):
+    return n ** 2
+    # or
+    return n * n
+
+
+if __name__ == "__main__":
+    print(square(2))  # 4
+    print(square(4))  # 16
+    print(square(-7))  # 49
