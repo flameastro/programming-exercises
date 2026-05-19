f34bf21c272b6a2f3b0c2e9e77f56eba3fe833d9
flameastro <gabrielgoulartbnu@gmail.com>
2026-05-19 20:26:50 -0300

    Create grasshopper-debug-sayhello

---
+++ codewars/python/8kyu/grasshopper-debug-sayhello/solution.py
@@ -0,0 +1,12 @@
+# Debugging sayHello function
+# The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+# Example output:
+# Hello, Mr. Spock
+def say_hello(name):
+    return f"Hello, {name}"
+
+
+if __name__ == "__main__":
+    print(say_hello("Stark"))  # Hello, Stark
+    print(say_hello("Iron Man"))  # Hello, Iron Man
+    print(say_hello("Bart"))  # Hello, Bart
